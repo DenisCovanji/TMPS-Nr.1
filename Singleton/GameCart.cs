7d88b31251279f5d4cd4fd06e8a9033af3d0f922
DenisCovanji <62745764+DenisCovanji@users.noreply.github.com>
2022-04-20 20:37:16 +0300

    Add files via upload

---
+++ Singleton/GameCart.cs
@@ -0,0 +1,29 @@
+﻿namespace Singleton
+{
+    class GameCart
+    {
+        private static GameCart _instance = null;
+
+        public int Games { get; set; }
+
+        private GameCart()
+        {
+            Console.WriteLine("The cart of games has been created. See the final price below.");
+        }
+
+        public static GameCart GetInstance()
+        {
+            if (_instance == null)
+            {
+                    if (_instance == null)
+                        _instance = new GameCart();
+            }
+            return _instance;
+        }
+        public void GetCartPrice(string price)
+        {
+            Console.WriteLine($"The final price is {price} dollars.");
+        }
+
+    }
+}
