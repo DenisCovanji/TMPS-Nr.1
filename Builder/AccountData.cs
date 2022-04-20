1e26daaa05f16fc3c52e0429dbefcdf1ef3f9a35
DenisCovanji <62745764+DenisCovanji@users.noreply.github.com>
2022-04-20 20:35:28 +0300

    Add files via upload

---
+++ Builder/AccountData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Account
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Gender { get; private set; }
+        public string Age { get; private set; }
+        public string Marital_status { get; private set; }
+
+
+        public static AccountBuilder With()
+        {
+            return new AccountBuilder();
+        }
+
+        public class AccountBuilder
+        {
+            private readonly Account _account = new Account();
+
+            public AccountBuilder Name(string name)
+            {
+                _account.Name = name;
+                return this;
+            }
+
+            public AccountBuilder Surname(string surname)
+            {
+                _account.Surname = surname;
+                return this;
+            }
+
+            public AccountBuilder Gender(string gender)
+            {
+                _account.Gender = gender;
+                return this;
+            }
+
+            public AccountBuilder Age(string age)
+            {
+                _account.Age = age;
+                return this;
+            }
+
+            public AccountBuilder Marital_status(string marital_status)
+            {
+                _account.Marital_status = marital_status;
+                return this;
+            }
+
+            public Account Build()
+            {
+                return _account;
+            }
+        }
+    }
+}
