a9b467277841b7117ab7df716ef9eeba40c244f5
DenisCovanji <62745764+DenisCovanji@users.noreply.github.com>
2022-04-22 10:21:53 +0300

    Add files via upload

---
+++ Builder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = Account.With()
+                .Name("John")
+                .Surname("Snow")
+                .Gender("Male")
+                .Age("25")
+                .Marital_status("Single")
+                .Build();
+
+            Console.ReadLine();
+        }
+    }
+}
